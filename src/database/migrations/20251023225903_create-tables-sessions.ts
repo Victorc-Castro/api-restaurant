88b373566a8cd267b25f588e553f9d0b97e5ec61
Victor <wortsac01@gmail.com>
2025-10-24 17:15:00 -0300

    openning table and creating pragma command

--- src/database/migrations/20251023225903_create-tables-sessions.ts
+++ src/database/migrations/20251023225903_create-tables-sessions.ts
@@ -16,6 +16,6 @@ export async function up(knex: Knex): Promise<void> {
 
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable("table_sessions")
+  await knex.schema.dropTable("tables_sessions")
 }
 
